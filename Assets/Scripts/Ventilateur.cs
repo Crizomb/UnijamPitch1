6d71418c349226040a3ff52d6a297e3048187b81
Milo Deroussi <miloderoussi@gmail.com>
2023-11-25 20:17:20 +0100

    ajout des ventilateurs

---
+++ Assets/Scripts/Ventilateur.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ventilateur : MonoBehaviour
+{
+
+    private float force;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("ok");
+        State state = collision.gameObject.GetComponent<PlayerState>().getState();
+        if (state == State.Solid)
+        {
+            force = 100f;
+        }
+        else if (state == State.Liquid)
+        {
+            force = 500f;
+        }
+        else if (state == State.Gas)
+        {
+            force = 1000f;
+        }
+        Debug.Log(force);
+        collision.attachedRigidbody.AddForce(new Vector2(0, 1f) * force);
+        
+    }
+
+}
