b4cc3e5943fadcf10d3c2341515b79bd2f2b3804
Crizomb <clementbart.45@gmail.com>
2023-11-25 10:58:34 +0100

    Add UI

--- Assets/Scripts/TempMeterScript.cs
+++ Assets/Scripts/TempMeterScript.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TempMeterScript : MonoBehaviour
 {
     public Temperature temperature;
-    private float yFactor = 1;
+    private float yFactor = 0.1f;
 
+    private GameObject roomTempGauge;
     private Vector3 initTempPos;
     // Start is called before the first frame update
     void Start()
     {
         // get the temperature script from the logic object
         temperature = GameObject.Find("Logic").GetComponent<Temperature>();
+        // get the room temperature gauge as the tag RoomTempGauge
+        roomTempGauge = GameObject.FindGameObjectWithTag("RoomTempGauge");
+
+        
         initTempPos = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float yScale = yFactor * temperature.slimeTemp*0.1f;
+        float yScale = yFactor * temperature.slimeTemp;
         transform.localScale = new Vector3(transform.localScale.x, yScale, transform.localScale.z);
 
         // Scale agrandit le thermometre vers le haut et vers le bas, donc il faut le deplacer vers le haut pour compenser
--- Assets/Scripts/Temperature.cs
+++ Assets/Scripts/Temperature.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Temperature : MonoBehaviour
 {
-    
+    // Get slider UI for the thermometer
+    [SerializeField]
+    private Slider thermometerSlider;
+
+    [SerializeField]
+    private GameObject RoomTemperatureGauge; // si Bug Drag and drop le GameObject RoomTemperatureGauge dans l'inspector (se trouve dans le canvas)
+
+    private RectTransform roomTemperatureGaugeRectTransform;
+
+
     public float roomTemp = 20; // Temperature de la salle
     public float slimeTemp; // Temperature du slime
     public float localTemp; // Temperature autour du slime (change entre zone chaude/froide)
@@ -14,6 +24,17 @@ public class Temperature : MonoBehaviour
     void Start()
     {
         localTemp = roomTemp;
+        roomTemperatureGaugeRectTransform = RoomTemperatureGauge.GetComponent<RectTransform>();
+
+        // rotation à 90° fait que x est la hauteur et y la largeur
+        float SliderHeight = thermometerSlider.GetComponent<RectTransform>().rect.width;
+        float SliderMaxValue = thermometerSlider.maxValue;
+        float TempGaugeHeight = SliderHeight * roomTemp / SliderMaxValue;
+        roomTemperatureGaugeRectTransform.anchoredPosition = new Vector2(TempGaugeHeight, 0);
+        Debug.Log("TempGaugeHeight : " + TempGaugeHeight);
+        Debug.Log("SliderHeight : " + SliderHeight);
+        Debug.Log("SliderMaxValue : " + SliderMaxValue);
+        
     }
 
     public void enterNewTempZone(float newLocalTemp)
@@ -30,5 +51,7 @@ public class Temperature : MonoBehaviour
     void Update()
     {
         slimeTemp += Time.deltaTime * heatTransferRate * (localTemp - slimeTemp);
+
+        thermometerSlider.value = slimeTemp;
     }
 }
