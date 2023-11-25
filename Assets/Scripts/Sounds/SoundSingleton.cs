4bf7122d8fa43f512a8d85a6576e373331c3015f
Isunia14 <94149685+Isunia14@users.noreply.github.com>
2023-11-25 17:04:36 +0100

    Sound System

---
+++ Assets/Scripts/Sounds/SoundSingleton.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SoundSingleton : MonoBehaviour
+{
+    public static SoundSingleton Instance = null;
+    
+    [Header("Musics")]
+    [SerializeField] private AudioClip MenuMusic;
+    [SerializeField] private AudioClip GameMusic;
+
+    [Header("Gameplay Sounds")]
+    [SerializeField] public AudioClip JumpSound;
+    [SerializeField] private AudioClip MoveSound;
+
+    [Header("UI Sounds")]
+    [SerializeField] private AudioClip ClickSound;
+    [SerializeField] private AudioClip WinSound;
+    [SerializeField] private AudioClip DieSound;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad (gameObject);
+    }
+
+    #region Music
+
+    public void PlayMusicMenu()
+    {
+        SoundManager.Instance.PlayMusic(MenuMusic);
+    }
+    
+    public void PlayMusicGame()
+    {
+        SoundManager.Instance.PlayMusic(GameMusic);
+    }
+
+    #endregion
+
+
+    #region Sounds
+
+    public void PlayJump()
+    {
+        SoundManager.Instance.PlaySound(JumpSound);
+    }
+    
+    public void PlayMove()
+    {
+        SoundManager.Instance.PlaySound(MoveSound);
+    }
+    
+    public void PlayClick()
+    {
+        SoundManager.Instance.PlaySound(ClickSound);
+    }
+    
+    public void PlayWin()
+    {
+        SoundManager.Instance.PlaySound(WinSound);
+    }
+    
+    public void PlayDie()
+    {
+        SoundManager.Instance.PlaySound(DieSound);
+    }
+    
+    #endregion
+}
