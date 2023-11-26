c95023a4c565c4a477d5b5f68b79332ef55b5c8f
Milo Deroussi <miloderoussi@gmail.com>
2023-11-26 10:55:36 +0100

    box pushable only in solid state

--- Assets/colision_manager.cs
+++ Assets/colision_manager.cs
@@ -6,23 +6,32 @@ public class colision_manager : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.GetComponent<PlayerState>().getState());
-        if (collision.gameObject.GetComponent<PlayerState>().getState()!= State.Solid)
-        {
-            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-        }
-        else
-        {
-            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        Debug.Log(collision.gameObject.GetComponentInParent<PlayerState>().getState() != State.Solid);
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player") {
+            if (collision.gameObject.GetComponentInParent<PlayerState>().getState() != State.Solid)
+            {
+                Debug.Log("ok");
+                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            }
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<PlayerState>().getState() != State.Solid)
+        if (collision.gameObject.tag == "Player")
         {
-            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            if (collision.gameObject.GetComponentInParent<PlayerState>().getState() != State.Solid)
+            {
+                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            }
         }
     }
+    
+    
     // Update is called once per frame
     void FixedUpdate()
     {
